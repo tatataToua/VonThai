7cf304db67c8a55750b61854a49838f0e296acb7
tatataToua <toualee10@gmail.com>
2025-02-11 17:12:33 -0500

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import "./App.css";
 import { Navbar, Home, Menu, Introduction, Footer, About } from "./components";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter  as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <main>
       <Routes>
         <Route
           path="/"
@@ -19,6 +20,7 @@ function App() {
         <Route path="/menu" element={<Menu />} />
         <Route path="/about" element={<About />} />
       </Routes>
+      </main>
       <Footer />
     </Router>
   );
